b66bdd21053f8add8e2ed1dfa50012128ebe05ba
Alex Bu <bkpfx000@gmail.com>
2014-01-09 23:21:16 +0800

    create folder

---
+++ mob123/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace mob123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://mobile.12306.cn");
+        }
+    }
+}
